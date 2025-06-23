ddd2e23ff11480695c90fc854d0b3a6e4b6b3b7f
Fabiano <fabianoquirinos@outlook.com>
2025-06-23 12:18:19 -0300

    :sparkles: feat: hello-world in Go

---
+++ hello-world/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// To run:go run <filename>.go
+// To build: go build-> creates an executable binary
+// go mod init name/of/module // import dependecys and versions controls
+
+func main() {
+	fmt.Println("Escrevendo o arquivo main")
+}
